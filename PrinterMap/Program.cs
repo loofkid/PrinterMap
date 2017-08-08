3ee920d0a62344cfbc7b4877cb4dab8ccc7be5c0
kloofbourrow <kloofbourrow@LTKLOOFBOURROW>
2017-08-08 11:20:39 -0400

    added error handling

--- PrinterMap/Program.cs
+++ PrinterMap/Program.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using IWshRuntimeLibrary;
+using System.Net.NetworkInformation;
+using System.Printing;
 
 namespace PrinterMap
 {
@@ -20,6 +22,7 @@ namespace PrinterMap
         private static string printershare;
         private static bool printerExist = false;
         private static Form1 progressBar = new Form1();
+        private static PingReply pingReply;
 
         static void Main(string[] args)
         {
@@ -28,53 +31,77 @@ namespace PrinterMap
             servername = printersplit[0];
             sharename = Uri.UnescapeDataString(printersplit[1]);
             printershare = "\\\\" + servername + "\\" + sharename;
+                      
             
-            BackgroundWorker bg = new BackgroundWorker();
-            bg.DoWork += new DoWorkEventHandler(bg_DoWork);
-            bg.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_RunWorkerCompleted);
-
-            
-
-            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            Ping ping = new Ping();
+            try { 
+                pingReply = ping.Send(servername);
+            } catch
             {
-                if (printershare == printer)
+                MessageBox.Show("Print server is not available.\nPlease make sure you can access Zeon's network.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (pingReply.Status == IPStatus.Success)
+            {
+                PrintServer printServer = new PrintServer(@"\\" + servername);
+                try
                 {
-                    printerExist = true;
+                    PrintQueue printQueue = printServer.GetPrintQueue(printershare);
+                } catch
+                {
+                    MessageBox.Show("Printer cannot be found on print server.\nPlease verify that printer exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
                 }
-            }
-            if (printerExist)
-            {
-                MessageBox.Show("Printer is already available on your computer.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            } else {
-                bg.RunWorkerAsync();
-                progressBar.ShowDialog();
-            }
-            void bg_DoWork(object sender, DoWorkEventArgs e)
-            {
-                WshNetwork printerAdd = new WshNetwork();
-                printerAdd.AddWindowsPrinterConnection(printershare);
-            }
-            void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-            {
-                do
+                BackgroundWorker bg = new BackgroundWorker();
+                bg.DoWork += new DoWorkEventHandler(bg_DoWork);
+                bg.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_RunWorkerCompleted);
+
+                foreach (string printer in PrinterSettings.InstalledPrinters)
                 {
-                    foreach (string printer in PrinterSettings.InstalledPrinters)
+                    if (printershare == printer)
                     {
-                        if (printershare == printer)
-                        {
-                            printerExist = true;
-                        }
+                        printerExist = true;
                     }
-                    Thread.Sleep(500);
-                } while (!printerExist);
-                progressBar.FormClosed += new FormClosedEventHandler(addComplete);
-                progressBar.Close();
-
-                void addComplete(object sender1, FormClosedEventArgs e1)
-                {
-                    MessageBox.Show("Printer has been successfully added!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-             }
+                if (printerExist)
+                {
+                    MessageBox.Show("Printer is already available on your computer.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    bg.RunWorkerAsync();
+                    progressBar.ShowDialog();
+                }
+                void bg_DoWork(object sender, DoWorkEventArgs e)
+                {
+                    WshNetwork printerAdd = new WshNetwork();
+                    printerAdd.AddWindowsPrinterConnection(printershare);
+                }
+                void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+                {
+                    do
+                    {
+                        foreach (string printer in PrinterSettings.InstalledPrinters)
+                        {
+                            if (printershare == printer)
+                            {
+                                printerExist = true;
+                            }
+                        }
+                        Thread.Sleep(500);
+                    } while (!printerExist);
+                    progressBar.FormClosed += new FormClosedEventHandler(addComplete);
+                    progressBar.Close();
+
+                    void addComplete(object sender1, FormClosedEventArgs e1)
+                    {
+                        MessageBox.Show("Printer has been successfully added!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            } else
+            {
+                MessageBox.Show("Print server is not available.\nPlease make sure you can access Zeon's network.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         
     }
